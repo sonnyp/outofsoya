d058c3f219227431d893eaf988b491a31105b795
Sonny Piers <sonny@fastmail.net>
2019-04-14 21:21:20 +0200

    enable npx preact build with prerender and production

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,7 @@
 import preactCliTypeScript from "preact-cli-plugin-typescript";
+import asyncPlugin from "preact-cli-plugin-async";
 
 export default function(config) {
   preactCliTypeScript(config);
+  asyncPlugin(config);
 }
